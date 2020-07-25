afedb9eeee85e20953f2ca30991585bf819cfc1e
vishal jain <vishal.jain_cs18@gla.ac.in>
2020-07-25 20:01:21 +0530

    Add files via upload

---
+++ data_types2.java
@@ -0,0 +1,22 @@
+package DATA_TYPES;
+
+public class d_t {
+    public static void main(String[] args) {
+        int a=8;
+        float b=12.99f;
+        double c=12.99d;
+        byte d=100;
+        short e=5000;
+        long f=120000000l;
+        char g='D';
+        boolean h=true;
+        System.out.println("a: " +a);
+        System.out.println("b: " +b);
+        System.out.println("c: " +c);
+        System.out.println("d: " +d);
+        System.out.println("e: " +e);
+        System.out.println("f: " +f);
+        System.out.println("g: " +g);
+        System.out.println("h: " +h);
+    }
+}

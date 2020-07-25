f74bf920f9c5fb1f820a523e09c7c9b9e9a71e6a
vishal jain <vishal.jain_cs18@gla.ac.in>
2020-07-25 19:42:11 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,15 @@
+package DATA_TYPES;
+
+public class data_types {
+    public static void main(String[] args) {
+        System.out.println("data types          size in bits            size in bytes                       range                \n");
+        System.out.println("byte                8 bits                  1 byte                         -128 to 127               ");
+        System.out.println("short               16 bits                 2 bytes                        -3278 to 32767            ");
+        System.out.println("int                 32 bits                 4 bytes                        -214783648 to 2147483647  ");
+        System.out.println("long                64 bits                 8 bytes                        -9223*10^15 to -9223*10^15");
+        System.out.println("float               32 bits                 4 bytes                        upto 7 decimal digits     ");
+        System.out.println("double              64 bits                 8 bytes                        upto 16 decimal digits    ");
+        System.out.println("char                16 bits                 2 bytes                        ASCII value 0 to 255      ");
+        System.out.println("boolean             1  bit                  1/8 bytes                      true and false            ");
+    }
+}
